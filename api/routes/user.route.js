1f552c6450b9f8942478e457a79fa8a27dde467d
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-10-15 19:28:21 -0700

    create user update api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { verifyToken } from "../utils/verifyUser.js";
+import { updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {});
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
