c6fea9db25c91a46633e673cb306caa9d025f1df
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-10-17 23:19:08 -0700

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -158,6 +158,26 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center">Profile</h1>
@@ -281,7 +301,10 @@ export default function Profile() {
               </Link>
 
               <div className="flex flex-col items-center">
-                <button className="text-red-700 text-sm uppercase">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 text-sm uppercase"
+                >
                   Delete
                 </button>
                 <button className="text-green-700 text-sm uppercase">
